5360525aab5b9c7be1eafe3756a69da0031af49f
ahmalla <68600561+ahmalla@users.noreply.github.com>
2023-01-09 09:53:04 -0800

    styling index.js

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,10 @@ export default function Home() {
           </nav>
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium'>Ahmad Mahmoud</h2>
-            <h3>Full-Stack Developer</h3>
-            <p>Amiable and dedicated software engineer with a background in Team Management and leadership. A punctual, organized and enthusiastic team player that isn’t afraid of a challenge and can help lead a group to success. Persistence keeps me curious and passionate about continually pursuing a better tomorrow. </p>
+            <h3 className='text-2xl py-2'>Full-Stack Developer</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>Amiable and dedicated software engineer with a background in Team Management and leadership. A punctual, organized and enthusiastic team player that isn’t afraid of a challenge and can help lead a group to success. Persistence keeps me curious and passionate about continually pursuing a better tomorrow. </p>
           </div>
-          <div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
             <a><AiFillGithub /></a>
             <a><AiFillLinkedin /></a>
           </div>
