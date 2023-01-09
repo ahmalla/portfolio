bc62a2b4eb14ea0e83f34662f60251ef77fa35fe
ahmalla <68600561+ahmalla@users.noreply.github.com>
2023-01-09 09:43:42 -0800

    added burtons text and imported react-icons for github and linkedin

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
-
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
 
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Ahmad's Portfolio</h1>
+            <h1 className='text-xl font-burtons'>Ahmad's Portfolio</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill className='cursor-pointer text-xl'/>
@@ -23,6 +23,15 @@ export default function Home() {
               <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
             </ul>
           </nav>
+          <div>
+            <h2>Ahmad Mahmoud</h2>
+            <h3>Full-Stack Developer</h3>
+            <p>Amiable and dedicated software engineer with a background in Team Management and leadership. A punctual, organized and enthusiastic team player that isn’t afraid of a challenge and can help lead a group to success. Persistence keeps me curious and passionate about continually pursuing a better tomorrow. </p>
+          </div>
+          <div>
+            <a><AiFillGithub /></a>
+            <a><AiFillLinkedin /></a>
+          </div>
         </section>
       </main>
     </>
