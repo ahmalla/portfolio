c6836d4b177732be2382f298aaf18c22325bc26f
ahmalla <68600561+ahmalla@users.noreply.github.com>
2023-01-09 09:16:33 -0800

    created nav bar with slight styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
 
 
@@ -11,9 +12,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className=' bg-cyan-700 h-screen'>
-          <h1>Hello</h1>
+      <main className='bg-white px-10'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>Ahmad's Portfolio</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-xl'/>
+              </li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+            </ul>
+          </nav>
         </section>
       </main>
     </>
