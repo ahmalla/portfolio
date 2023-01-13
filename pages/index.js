8586519603f4b4d935d6c58d52901c8dd84422c0
ahmalla <68600561+ahmalla@users.noreply.github.com>
2023-01-13 08:03:00 -0800

    styling for mobile

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-white px-10'>
+      <main className='bg-white px-10 md:px-20 lg:px-40'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
             <h1 className='text-xl font-burtons'>Developed By Ahmad</h1>
